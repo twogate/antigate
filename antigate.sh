b6f8a1ad908e3b878e0a179855121dd778f3941e
keiya <keiya_21@yahoo.co.jp>
2019-04-09 08:58:34 +0900

    doller

--- antigate.sh
+++ antigate.sh
@@ -5,15 +5,13 @@ host="$(basename $HOME)@sakura"
 tmptext='/tmp/antigate_tmp.txt'
 jsonfile='/tmp/antigate_upload.json'
 
-olddigest="$(echo $PASS | gpg -d --cipher-algo AES256 --batch --yes --passphrase-fd 0 \"${digest_path}.gpg\")"
+olddigest=$"$(echo $PASS | gpg -d --cipher-algo AES256 --batch --yes --passphrase-fd 0 \"${digest_path}.gpg\")"
 
 # hash check
 hashcheck="$(echo -e "$olddigest" | /usr/local/bin/shasum -c | grep FAILED)"
-newdigest="$(find $target_path -type f \( -name '*.php' -o -name '*.cgi' -o -name '*.shtml' -o -name '*.shtm' -o -name '.htaccess' \) -print0 | xargs -0 /usr/local/bin/shasum)"
-echo $olddigest
-echo $newdigest
+newdigest=$"$(find $target_path -type f \( -name '*.php' -o -name '*.cgi' -o -name '*.shtml' -o -name '*.shtm' -o -name '.htaccess' \) -print0 | xargs -0 /usr/local/bin/shasum)"
 
-diff="$(diff -u <(echo -e \"$olddigest\") <(echo -e \"$newdigest\"))"
+diff="$(diff -u <(echo -e $\"$olddigest\") <(echo -e $\"$newdigest\"))"
 
 echo -e "$newdigest" > "$digest_path"
 echo $PASS | gpg -c --cipher-algo AES256 --batch --yes --passphrase-fd 0 "$digest_path"

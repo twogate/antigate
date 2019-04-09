dcebf90d0cbbdedb77aebd8bc327da132b11a4a2
keiya <keiya_21@yahoo.co.jp>
2019-04-09 09:06:18 +0900

    newline

--- antigate.sh
+++ antigate.sh
@@ -11,7 +11,7 @@ olddigest=$(echo $PASS | gpg -d --cipher-algo AES256 --batch --yes --passphrase-
 hashcheck="$(echo -e "$olddigest" | /usr/local/bin/shasum -c | grep FAILED)"
 newdigest=$(find $target_path -type f \( -name '*.php' -o -name '*.cgi' -o -name '*.shtml' -o -name '*.shtm' -o -name '.htaccess' \) -print0 | xargs -0 /usr/local/bin/shasum)
 
-diff="$(diff -u <(echo -e $olddigest) <(echo -e $newdigest))"
+diff="$(diff -u <(echo -e "$olddigest") <(echo -e "$newdigest"))"
 
 echo -e "$newdigest" > "$digest_path"
 echo $PASS | gpg -c --cipher-algo AES256 --batch --yes --passphrase-fd 0 "$digest_path"
@@ -19,7 +19,7 @@ rm -f "$digest_path"
 
 date="$(date)"
 
-printf '```\n---- %s %s ----\nSHA1 Check result:\n%s\n\n\nChanges:\n%s\n```' "$host" "$date" "$hashcheck" "$diff" > $tmptext
+printf '```\n---- %s %s ----\nSHA1 Check result:\n%s\n\n\nChanges:\n%s\n```' "$host" "$date" "$hashcheck" $"$diff" > $tmptext
 
 # build json file
 echo "{\"channel\": \"#front-alert\", \"username\": \"${host}\", \"text\": \"" > $jsonfile
